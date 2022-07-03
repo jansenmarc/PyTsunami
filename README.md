b7a32baded536a0f8757ec719f6a538a1d987c6d
git <jansen.marc@web.de>
2022-07-03 18:21:00 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # PyTsunami
-A python library for interacting with the Tsunami Exchange
+A simple python library for interacting with the Tsunami Exchange
+## Available methods
+- calcRemainMarginWithFundingPayment(positionSize, positionMargin, positionLstUpdCPF, unrealizedPnl) - Calculation of the remaining maring including funding payments
+- getPositionNotionalAndUnrealizedPnl(address)- Calculation of the positions notional and unrealized Pnl
+- getPosition(address) - Getting the position of a given address
+- getPayout(address) - Getting the payout for a given address
+- getTwapSpotPrice() - Getting the price of the pair on the Tsunami Exchange
+- getOracleTwapPrice() - Getting the oracle price of the pair
+- getFundingRate() - Calculating the current funding rate
+- getTimeToNextFunding() - Calculating the time till next funding payments
+- getNextFundingTimestamp() - Getting the timestamp of the next funding payments
+- getDataFromContract(key) - Getting data entries from the contract (mainly used internally)
+- getDataFromAddress(address, key) - Getting all datas from the contract (mostly used internally)
+- liquidate(address) - Liquidate a given address
+- long(investment, margin) - Going long on the defined trading pair with the presented amount (investment) and presented margin
+- decreaseLong(investment, margin) - Decreasing a position by the given amount (investment) and presented margin
+- short(investment, margin) - Going short on the defined trading pair with the presented amount (investment) and presented margin
+- decreaseShort(investment, margin) - Increasing a position by the given amount (investment) and presented margin
+- closePosition() - Closing the position of the current address
+## Examples
+Instantiation of the class for testnet:
+```python
+from tsunami import Tsunami
+import pywaves as pw
+
+# configure a testnet node
+node = 'https://nodes-testnet.wavesnodes.com'
+# asset id of USDN on testnet
+usdnAssetId = 'HezsdQuRDtzksAYUy97gfhKy7Z1NW2uXYSHA3bgqenNZ'
+# create an address, especially necessary if you want to go long or short
+pw.setNode(node, 'T')
+myAddress = pw.Address(seed='this is just a test seed')
+# configure the address for the Tsunami market you want to interact with, e.g. the WAVES/USDN market
+tsunamiContractAddress = '3N4mv2c2ehFvfSR5pXDCUqFZDaatagfBaMA'
+
+tsunami = Tsunami(tsunamiContractAddress, myAddress = myAddress, node = node, usdnAssetId = usdnAssetId)
+```
+## Interacting with Tsunami markets
+Once you have created an instance of the Tsunami class, you can use it to get information about the market by calling the corresponding methods.
+```python
+tsunami.getTwapSpotPrice()
+```
---
+++ tsunami.py
@@ -0,0 +1,94 @@
+import requests
+from datetime import datetime
+
+class Tsunami:
+
+    def __init__(self, contractAddress, myAddress = None, node = 'https://nodes.wavesexplorer.com', usdnAssetId = 'DG2xFkPdDwKUoBkzGAhQtLpSGzfXLiCYPEzeKH2Ad24p'):
+        self.contractAddress = contractAddress
+        self.usdnAssetId = usdnAssetId
+        self.myAddress = myAddress
+        self.node = node
+        self.LONG = 1
+        self.SHORT = 2
+
+    def calcRemainMarginWithFundingPayment(self, positionSize, positionMargin, positionLstUpdCPF, unrealizedPnl):
+        position = requests.post(self.node + '/utils/script/evaluate/' + self.contractAddress, json = { "expr": "calcRemainMarginWithFundingPayment(" + str(positionSize) + ", "  + str(positionMargin) + ", " + str(positionLstUpdCPF) + ", " + str(unrealizedPnl) + ")" }).json()
+        positionValues = position['result']['value']
+
+        return { 'remainMargin': positionValues['_1']['value'], 'badDebt': positionValues['_2']['value'] }
+
+    def getPositionNotionalAndUnrealizedPnl(self, address):
+        position = requests.post(self.node + '/utils/script/evaluate/' + self.contractAddress, json = { "expr": "getPositionNotionalAndUnrealizedPnl(\"" + address + "\")" }).json()
+        positionValues = position['result']['value']
+
+        return { 'positionNotional': positionValues['_1']['value'], 'unrealizedPnl': positionValues['_2']['value'] }
+
+    def getPosition(self, address):
+        position = requests.post(self.node + '/utils/script/evaluate/3N4mv2c2ehFvfSR5pXDCUqFZDaatagfBaMA', json = { "expr": "getPosition(\"" + address + "\")" }).json()
+        positionValues = position['result']['value']
+
+        return { 'positionSize': positionValues['_1']['value'], 'margin': positionValues['_2']['value'], 'pon': positionValues['_3']['value'], 'positionLstUpdCPF': positionValues['_4']['value'] }
+
+    def getPayout(self, address):
+        position = self.getPosition(address)
+        notionalAndUnrealizedPnl = self.getPositionNotionalAndUnrealizedPnl(address)
+        remainMarginWithFundingPayment = self.calcRemainMarginWithFundingPayment(position['positionSize'], position['margin'], position['positionLstUpdCPF'], notionalAndUnrealizedPnl['unrealizedPnl'])
+
+        return remainMarginWithFundingPayment['remainMargin'] / 1000000
+
+    def getTwapSpotPrice(self):
+        qtAstR = self.getDataFromContract('k_qtAstR')
+        bsAstR = self.getDataFromContract('k_bsAstR')
+
+        return qtAstR / bsAstR
+
+    def getOracleTwapPrice(self):
+        oracle = self.getDataFromContract('k_ora')
+        priceKey = self.getDataFromContract('k_ora_key')
+
+        return self.getDataFromAddress(oracle, priceKey) / 1000000
+
+    def getFundingRate(self):
+        underlyingPrice = self.getOracleTwapPrice()
+        spotTwapPrice = self.getTwapSpotPrice()
+        premium = spotTwapPrice - underlyingPrice
+        decimalUnit = (1 * (((((10 * 10) * 10) * 10) * 10) * 10))
+        oneDay = 86400 * decimalUnit
+        fundingPeriodRaw = self.getDataFromContract('k_fundingPeriod')
+        fundingPeriodDecimal = fundingPeriodRaw * decimalUnit
+        premiumFraction = (premium * fundingPeriodDecimal) / oneDay
+
+        return premiumFraction / underlyingPrice
+
+    def getTimeToNextFunding(self):
+        nextFundingRound = self.getNextFundingTimestamp()
+        now = datetime.now()
+
+        return nextFundingRound - now
+
+    def getNextFundingTimestamp(self):
+        return self.getDataFromContract('k_nextFundingBlockMinTimestamp')
+
+    def getDataFromContract(self, key):
+        return self.getDataFromAddress(self.contractAddress, key)
+
+    def getDataFromAddress(self, address, key):
+        return requests.get(self.node + '/addresses/data/' + address + '/' + key).json()['value']
+
+    def liquidate(self, address):
+        return self.myAddress.invokeScript(self.contractAddress, 'liquidate', [{'type': 'string', 'value': address}], [])
+
+    def long(self, investment, margin):
+        return self.myAddress.invokeScript(self.contractAddress, 'increasePosition', [{'type': 'integer', 'value': self.LONG}, {'type': 'integer', 'value': margin * 1000000}, {'type': 'integer', 'value': int(investment / 4 * 1000000)}], [ { "amount": investment * 1000000, "assetId": self.usdnAssetId }])
+
+    def decreaseLong(self, investment, margin):
+        return self.myAddress.invokeScript(self.contractAddress, 'decreasePosition', [{'type': 'integer', 'value': self.LONG}, {'type': 'integer', 'value': investment * 1000000 }, {'type': 'integer', 'value': margin * 1000000}, {'type': 'integer', 'value': int(investment / 2 * 1000000)}], [ ])
+
+    def short(self, investment, margin):
+        return self.myAddress.invokeScript(self.contractAddress, 'increasePosition', [{'type': 'integer', 'value': self.SHORT}, {'type': 'integer', 'value': margin * 1000000}, {'type': 'integer', 'value': int(investment / 4 * 1000000)}], [ { "amount": investment * 1000000, "assetId": self.usdnAssetId }])
+
+    def decreaseShort(self, investment, margin):
+        return self.myAddress.invokeScript(self.contractAddress, 'decreasePosition', [{'type': 'integer', 'value': self.SHORT}, {'type': 'integer', 'value': investment * 1000000}, {'type': 'integer', 'value': margin * 1000000}, {'type': 'integer', 'value': int(investment / 2 * 1000000)}], [ ])
+
+    def closePosition(self):
+        return self.myAddress.invokeScript(self.contractAddress, 'closePosition', [], [])
